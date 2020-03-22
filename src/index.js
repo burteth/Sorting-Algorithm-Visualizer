01442fa10994fad692c1146a29cd5229a2a786ff
burteth <ethanb127@gmail.com>
2020-03-21 20:47:51 -0400

    Add vertical bars to UI

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Bars extends React.Component {
+  render() {
+    var rows = [];
+    for (var i = 0; i < this.props.values.length; i++) {
+      rows.push(<div id="bar" style={{ height: this.props.values[i] * 5 }} />);
+    }
+    return rows;
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class SortButton extends React.Component {
+  render() {
+    return <button onClick={() => this.props.randomizebars()}>Sort</button>;
+  }
+}
+
+class Visualizer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      bar_values: GenerateBars(150)
+    };
+  }
+  randomizebars() {
+    this.setState({ bar_values: GenerateBars(150) });
+  }
+  render() {
+    return (
+      <div id="UI_container">
+        <div id="button_container">
+          <SortButton randomizebars={() => this.randomizebars()} />
+        </div>
+        <div id="bar_container">
+          <Bars values={this.state.bar_values} />
+        </div>
+      </div>
+    );
+  }
+}
+
+function GenerateBars(len, min, max) {
+  var lst = [];
+  for (var i = 0; i < len; i++) {
+    lst.push(getRandomInt(5, 150));
+  }
+
+  return lst;
+}
+
+ReactDOM.render(<Visualizer />, document.getElementById("root"));
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
