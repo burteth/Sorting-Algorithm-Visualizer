6bc43c27cb2b46a888654d63b8a681b153abb02e
burteth <ethanb127@gmail.com>
2020-05-02 16:30:14 -0400

    Create production build

---
+++ build/precache-manifest.d94651198905829ae4fd0825917ebc0f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc2e062158d3e7b8a7b34c4d8c7a007",
+    "url": "/Sorting-Algorithm-Visualizer/index.html"
+  },
+  {
+    "revision": "4b4f12cb16540d6c2ff4",
+    "url": "/Sorting-Algorithm-Visualizer/static/css/main.ba54d865.chunk.css"
+  },
+  {
+    "revision": "ade6d5c7350a00525c77",
+    "url": "/Sorting-Algorithm-Visualizer/static/js/2.fcebe8e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sorting-Algorithm-Visualizer/static/js/2.fcebe8e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b4f12cb16540d6c2ff4",
+    "url": "/Sorting-Algorithm-Visualizer/static/js/main.87e44764.chunk.js"
+  },
+  {
+    "revision": "c503b572d254a901c38f",
+    "url": "/Sorting-Algorithm-Visualizer/static/js/runtime-main.b949485b.js"
+  },
+  {
+    "revision": "84e1dfcb7c02a089636ef1728795429f",
+    "url": "/Sorting-Algorithm-Visualizer/static/media/CircularStd-Black.84e1dfcb.woff"
+  },
+  {
+    "revision": "f8b79adbe3829c1671005e3523591172",
+    "url": "/Sorting-Algorithm-Visualizer/static/media/CircularStd-BlackItalic.f8b79adb.woff"
+  },
+  {
+    "revision": "d39532612ce8f1c860fb0ad140f4d4fd",
+    "url": "/Sorting-Algorithm-Visualizer/static/media/CircularStd-Bold.d3953261.woff"
+  },
+  {
+    "revision": "632051af2c767cc657218f8a66db8537",
+    "url": "/Sorting-Algorithm-Visualizer/static/media/CircularStd-BoldItalic.632051af.woff"
+  },
+  {
+    "revision": "3d25cc45abef05651c4c02a9276fe7ad",
+    "url": "/Sorting-Algorithm-Visualizer/static/media/CircularStd-Book.3d25cc45.woff"
+  },
+  {
+    "revision": "fc6bf0edd4d5985d18be68bde5d5ecaa",
+    "url": "/Sorting-Algorithm-Visualizer/static/media/CircularStd-BookItalic.fc6bf0ed.woff"
+  },
+  {
+    "revision": "ae7c06fc499add195113c5bc5e47542c",
+    "url": "/Sorting-Algorithm-Visualizer/static/media/CircularStd-Medium.ae7c06fc.woff"
+  },
+  {
+    "revision": "df2ef3fc263438e55a1af8d632042cca",
+    "url": "/Sorting-Algorithm-Visualizer/static/media/CircularStd-MediumItalic.df2ef3fc.woff"
+  }
+]);
